a0c845eb700ba0c2f5536469d6a40f78ae590dd3
Max Amey <maxwellamey@gmail.com>
2016-10-17 11:14:40 -0500

    Deleted line in lastQuestionButtonHandler to fix the bug that was stopping the final results modal from showing up. Added an Index component and set up the route in build.js

--- build.js
+++ build.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import Create from './lib/Create'
 import Active from './lib/ActiveGame'
+import Index from './lib/Index'
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ Create }/>
+    <Route path="/" component={ Index }/>
+    <Route path="/create" component={ Create }/>
     <Route path="/active/:game_id" component={ Active }/>
   </Router>
 ), document.getElementById('app'))
