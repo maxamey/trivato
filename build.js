76b83dbe8981cfaf9aedd234549c1356ea5ee50c
Max Amey <maxwellamey@gmail.com>
2016-10-17 15:17:20 -0500

    separated the list of available games to simplify the choices in the index

--- build.js
+++ build.js
@@ -4,10 +4,12 @@ import { Router, Route, hashHistory } from 'react-router'
 import Create from './lib/Create'
 import Active from './lib/ActiveGame'
 import Index from './lib/Index'
+import Games from './lib/Games'
 
 render((
   <Router history={ hashHistory }>
     <Route path="/" component={ Index }/>
+    <Route path="/games" component={ Games }/>
     <Route path="/create" component={ Create }/>
     <Route path="/active/:game_id" component={ Active }/>
   </Router>
