8f2918d7d60ab9c06c6d4a8d7b2dddf9e22655f0
Max Amey <maxwellamey@gmail.com>
2016-10-17 15:33:24 -0500

    Added a nav bar that is a parent to all other components for easy navigation through the features.

--- build.js
+++ build.js
@@ -5,12 +5,15 @@ import Create from './lib/Create'
 import Active from './lib/ActiveGame'
 import Index from './lib/Index'
 import Games from './lib/Games'
+import Nav from './lib/Nav'
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ Index }/>
-    <Route path="/games" component={ Games }/>
-    <Route path="/create" component={ Create }/>
-    <Route path="/active/:game_id" component={ Active }/>
+    <Route path="/nav" component={ Nav }>
+      <Route path="/" component={ Index }/>
+      <Route path="/games" component={ Games }/>
+      <Route path="/create" component={ Create }/>
+      <Route path="/active/:game_id" component={ Active }/>
+    </Route>
   </Router>
 ), document.getElementById('app'))
